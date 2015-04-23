6d0a61e8ad6a14fe6dc337ed0210a8b0b05ae78c
wayne hu <iamwaynehu@gmail.com>
2015-04-23 13:45:59 +0800

    Wayne:[C]Use pointer to pointer

--- C/pointer_to_pointer.c
+++ C/pointer_to_pointer.c
@@ -8,12 +8,22 @@ int main()
     char *b="world";
     char *c="huwei";
     char *d="ctxrr";
-    char *p[4]={a,b,c,d};
-    char **q=p;
 
+    /*"p" is a pointer to pointer array*/
+    char *p[4]={a,b,c,d};
+    for (i = 0; i < sizeof(*p); i++)
+    {
+        printf("%s\n", *(p+i));
+    }
+
+    printf("\n");
+
+    /*"q" is a pointer to pointer*/
+    char **q=p;
     for (i = 0; i < sizeof(*q); i++)
     {
         printf("%s\n", *q++);
     }
+
     return 0;
 }
