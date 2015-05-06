5452cf8d8cde777309652b8f8458ba4cf7bf091f
WayneHu <iamwaynehu@gmail.com>
2015-05-06 15:00:53 +0800

    Wayne:[Python] homework in ArrayStack     homework:C-6.16

--- python/ch6/ArrayStack.py
+++ python/ch6/ArrayStack.py
@@ -4,12 +4,17 @@ class Empty(Exception):
     """Error attempting to access an element from an empty container"""
     pass
 
+class Full(Exception):
+    """Error attempting to access an element from an full container"""
+    pass
+
 class ArrayStack:
     """LIFO Stack implementation using a Python list as underlying storage"""
 
-    def __init__(self):
+    def __init__(self,maxlen=None):
         """Create an empty stack."""
         self._data = []
+        self._maxlen=maxlen
 
     def __len__(self):
         """Return the number of elements in the stack."""
@@ -21,6 +26,9 @@ class ArrayStack:
 
     def push(self,e):
         """Add element e to the top of the stack"""
+        if self._maxlen!=None:
+            if len(self._data)==self._maxlen:
+                raise Full('The stack is full')
         self._data.append(e)
 
     def top(self):
