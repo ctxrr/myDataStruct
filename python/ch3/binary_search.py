4b6eac1bc6c0d36cf46dbc4d6dc96f690cb188c6
WayneHu <iamwaynehu@gmail.com>
2015-05-15 16:11:12 +0800

    Wayne:[Python] re-implement 2 function in nonrecursion way

--- python/ch3/binary_search.py
+++ python/ch3/binary_search.py
@@ -13,8 +13,25 @@ def binary_search(data, target, low, high):
 		else:
 			return binary_search(data, target, mid + 1, high)
 
+def binary_search_iterable(data,target):
+	"""the old binary_search is tail recursion,so it can be implemented in nonrecursive way"""
+	low=0
+	high=len(data)-1
+
+	while low <= high:
+		mid = (low + high) // 2
+		if target == data[mid]:
+			return True
+		elif target < data[mid]:
+			high = mid - 1
+		else:
+			low = mid + 1
+	return False
+
 if __name__ == '__main__':
 	testlist=[]
 	for i in range(100):
 		testlist.append(i)
 	print binary_search(testlist,0,0,99)
+
+	print binary_search_iterable(testlist,1)
--- python/ch3/reverse.py
+++ python/ch3/reverse.py
@@ -5,6 +5,13 @@ def reverse(S,start=None,stop=None):
 		S[start],S[stop] = S[stop],S[start]
 		reverse(S,start+1,stop-1)
 
+def reverse_iterative(S):
+	"""Reverse elements in sequence S."""
+	start, stop = 0, len(S)
+	while start < stop - 1:
+		S[start], S[stop-1] = S[stop-1], S[start]
+		start, stop = start + 1, stop - 1
+
 if __name__ == '__main__':
 	testlist=[]
 	for i in range(10):
@@ -12,3 +19,11 @@ if __name__ == '__main__':
 	print testlist
 	reverse(testlist,0,9)
 	print testlist
+
+	print '--------------------------------'
+	testlist1=[]
+	for i in range(10):
+		testlist1.append(i)
+	print testlist1
+	reverse_iterative(testlist1)
+	print testlist1
