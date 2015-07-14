7a3f573088616222552b8bf23b51f417641149d0
waynehu <iamwaynehu@gmail.com>
2015-07-14 22:36:23 +0800

    Wayne:[Ch8]Modify Tree class

--- python/ch8/Tree.py
+++ python/ch8/Tree.py
@@ -49,3 +49,29 @@ class Tree:
     def is_empty(self):
         """Return True if the tree is empty."""
         return len(self) == 0
+
+    def depth(self, p):
+        """Return the number of levels separating Position p from the root."""
+        if self.is_root(p):
+            return 0
+        else:
+            return 1 + self.depth(self.parent(p))
+
+    def _height1(self): # works, but O(nˆ2) worst-case time
+        """Return the height of the tree."""
+        return max(self.depth(p) for p in self.positions() if self.is_leaf(p))
+
+    def _height2(self, p): # time is linear in size of subtree
+        """Return the height of the subtree rooted at Position p."""
+        if self.is_leaf(p):
+            return 0
+        else:
+            return 1 + max(self. height2(c) for c in self.children(p))
+
+    def height(self, p=None):
+        """Return the height of the subtree rooted at Position p.
+           If p is None, return the height of the entire tree.
+        """
+        if p is None:
+            p = self.root()
+        return self._height2(p) # start height2 recursion
