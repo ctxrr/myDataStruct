91c4ea0fcd85b79bc47ddf56c1f1e08abdad21a8
wayne hu <iamwaynehu@gmail.com>
2015-07-15 15:03:26 +0800

    Wayne:Modify the indent size from 2 into 4

--- python/ch8/Tree.py
+++ python/ch8/Tree.py
@@ -1,85 +1,85 @@
 class Tree:
-  """Abstract base class representing a tree structure."""
+    """Abstract base class representing a tree structure."""
 
-  #------------------------------- nested Position class -------------------------------
-  class Position:
-    """An abstraction representing the location of a single element within a tree.
+    #------------------------------- nested Position class -------------------------------
+    class Position:
+        """An abstraction representing the location of a single element within a tree.
 
-    Note that two position instaces may represent the same inherent location in a tree.
-    Therefore, users should always rely on syntax 'p == q' rather than 'p is q' when testing
-    equivalence of positions.
-    """
+        Note that two position instaces may represent the same inherent location in a tree.
+        Therefore, users should always rely on syntax 'p == q' rather than 'p is q' when testing
+        equivalence of positions.
+        """
 
-    def element(self):
-      """Return the element stored at this Position."""
-      raise NotImplementedError('must be implemented by subclass')
-      
-    def __eq__(self, other):
-      """Return True if other Position represents the same location."""
-      raise NotImplementedError('must be implemented by subclass')
+        def element(self):
+            """Return the element stored at this Position."""
+            raise NotImplementedError('must be implemented by subclass')
 
-    def __ne__(self, other):
-      """Return True if other does not represent the same location."""
-      return not (self == other)            # opposite of __eq__
+        def __eq__(self, other):
+            """Return True if other Position represents the same location."""
+            raise NotImplementedError('must be implemented by subclass')
 
-  # ---------- abstract methods that concrete subclass must support ----------
-  def root(self):
-    """Return Position representing the tree's root (or None if empty)."""
-    raise NotImplementedError('must be implemented by subclass')
+        def __ne__(self, other):
+            """Return True if other does not represent the same location."""
+            return not (self == other)            # opposite of __eq__
 
-  def parent(self, p):
-    """Return Position representing p's parent (or None if p is root)."""
-    raise NotImplementedError('must be implemented by subclass')
+    # ---------- abstract methods that concrete subclass must support ----------
+    def root(self):
+        """Return Position representing the tree's root (or None if empty)."""
+        raise NotImplementedError('must be implemented by subclass')
 
-  def num_children(self, p):
-    """Return the number of children that Position p has."""
-    raise NotImplementedError('must be implemented by subclass')
+    def parent(self, p):
+        """Return Position representing p's parent (or None if p is root)."""
+        raise NotImplementedError('must be implemented by subclass')
 
-  def children(self, p):
-    """Generate an iteration of Positions representing p's children."""
-    raise NotImplementedError('must be implemented by subclass')
+    def num_children(self, p):
+        """Return the number of children that Position p has."""
+        raise NotImplementedError('must be implemented by subclass')
 
-  def __len__(self):
-    """Return the total number of elements in the tree."""
-    raise NotImplementedError('must be implemented by subclass')
+    def children(self, p):
+        """Generate an iteration of Positions representing p's children."""
+        raise NotImplementedError('must be implemented by subclass')
 
-  # ---------- concrete methods implemented in this class ----------
-  def is_root(self, p):
-    """Return True if Position p represents the root of the tree."""
-    return self.root() == p
+    def __len__(self):
+        """Return the total number of elements in the tree."""
+        raise NotImplementedError('must be implemented by subclass')
 
-  def is_leaf(self, p):
-    """Return True if Position p does not have any children."""
-    return self.num_children(p) == 0
+    # ---------- concrete methods implemented in this class ----------
+    def is_root(self, p):
+        """Return True if Position p represents the root of the tree."""
+        return self.root() == p
 
-  def is_empty(self):
-    """Return True if the tree is empty."""
-    return len(self) == 0
+    def is_leaf(self, p):
+        """Return True if Position p does not have any children."""
+        return self.num_children(p) == 0
 
-  def depth(self, p):
-    """Return the number of levels separating Position p from the root."""
-    if self.is_root(p):
-      return 0
-    else:
-      return 1 + self.depth(self.parent(p))
+    def is_empty(self):
+        """Return True if the tree is empty."""
+        return len(self) == 0
 
-  def _height1(self):                 # works, but O(n^2) worst-case time
-    """Return the height of the tree."""
-    return max(self.depth(p) for p in self.positions() if self.is_leaf(p))
+    def depth(self, p):
+        """Return the number of levels separating Position p from the root."""
+        if self.is_root(p):
+            return 0
+        else:
+            return 1 + self.depth(self.parent(p))
 
-  def _height2(self, p):                  # time is linear in size of subtree
-    """Return the height of the subtree rooted at Position p."""
-    if self.is_leaf(p):
-      return 0
-    else:
-      return 1 + max(self._height2(c) for c in self.children(p))
+    def _height1(self):                 # works, but O(n^2) worst-case time
+        """Return the height of the tree."""
+        return max(self.depth(p) for p in self.positions() if self.is_leaf(p))
 
-  def height(self, p=None):
-    """Return the height of the subtree rooted at Position p.
+    def _height2(self, p):                  # time is linear in size of subtree
+        """Return the height of the subtree rooted at Position p."""
+        if self.is_leaf(p):
+            return 0
+        else:
+            return 1 + max(self._height2(c) for c in self.children(p))
 
-    If p is None, return the height of the entire tree.
-    """
-    if p is None:
-      p = self.root()
-    return self._height2(p)        # start _height2 recursion
+    def height(self, p=None):
+        """Return the height of the subtree rooted at Position p.
+
+        If p is None, return the height of the entire tree.
+        """
+        if p is None:
+            p = self.root()
+        return self._height2(p)        # start _height2 recursion
 
