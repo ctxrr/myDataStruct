977f1809d2df46bf4041c81d1cb85283a73f6e6f
wayne hu <iamwaynehu@gmail.com>
2015-07-15 14:49:52 +0800

    Wayne:Re-init the example code of Tree Binarytree Linkedbinarytree

--- python/ch8/BinaryTree.py
+++ python/ch8/BinaryTree.py
@@ -1,34 +1,38 @@
+from Tree import Tree
+
 class BinaryTree(Tree):
-    """Abstract base class representing a binary tree structure."""
+  """Abstract base class representing a binary tree structure."""
 
-    # --------------------- additional abstract methods ---------------------
-    def left(self, p):
-        """Return a Position representing p s left child.
-        Return None if p does not have a left child.
-        """
-        raise NotImplementedError('must be implemented by subclass')
+  # --------------------- additional abstract methods ---------------------
+  def left(self, p):
+    """Return a Position representing p's left child.
 
-    def right(self, p):
-        """Return a Position representing p s right child.
-        Return None if p does not have a right child.
-        """
-        raise NotImplementedError('must be implemented by subclass')
+    Return None if p does not have a left child.
+    """
+    raise NotImplementedError('must be implemented by subclass')
 
-    # ---------- concrete methods implemented in this class ----------
-    def sibling(self, p):
-        """Return a Position representing p s sibling (or None if no sibling)."""
-        parent = self.parent(p)
-        if parent is None: # p must be the root
-            return None # root has no sibling
-        else:
-            if p == self.left(parent):
-                return self.right(parent) # possibly None
-            else:
-                return self.left(parent) # possibly None
+  def right(self, p):
+    """Return a Position representing p's right child.
 
-    def children(self, p):
-        """Generate an iteration of Positions representing p s children."""
-        if self.left(p) is not None:
-            yield self.left(p)
-        if self.right(p) is not None:
-            yield self.right(p)
+    Return None if p does not have a right child.
+    """
+    raise NotImplementedError('must be implemented by subclass')
+
+  # ---------- concrete methods implemented in this class ----------
+  def sibling(self, p):
+    """Return a Position representing p's sibling (or None if no sibling)."""
+    parent = self.parent(p)
+    if parent is None:                    # p must be the root
+      return None                         # root has no sibling
+    else:
+      if p == self.left(parent):
+        return self.right(parent)         # possibly None
+      else:
+        return self.left(parent)          # possibly None
+
+  def children(self, p):
+    """Generate an iteration of Positions representing p's children."""
+    if self.left(p) is not None:
+      yield self.left(p)
+    if self.right(p) is not None:
+      yield self.right(p)
