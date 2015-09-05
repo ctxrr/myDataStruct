0b05898f453d393e3ed9ab11756bc8ed1c00214c
waynehu <iamwaynehu@gmail.com>
2015-09-05 16:41:34 +0800

    Wayne:Re-implemented Tree ADT and Binarytree ADT ,TBC

--- C++/Tree/BinaryTree.h
+++ C++/Tree/BinaryTree.h
@@ -1,26 +1,64 @@
+/**
+  ******************************************************************************
+  * @file    BinaryTree.h
+  * @author  Wayne Hu
+  * @date    2015.09.05
+  * @brief   This file provides all the headers of the menu functions.
+  ******************************************************************************
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef BINARYTREE_H_
 #define BINARYTREE_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include <iostream>
+#include "Tree.h"
+
+/* Exported macro ------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+/* Class delcaration ---------------------------------------------------------*/
+/* Template definition -------------------------------------------------------*/
+
 template<typename T>
-class BinaryTree {
-    private:
-        Node root;
-        int num;
-        class Node {
-            private:
-                Node *parent,left,right;
-                T item;
-            public:
-                Node();
-        };
-        Node::Node() {
-
-        }
+class BTNode : public TNode<T>
+{
     public:
-        BinaryTree();
-        void add_left(T &item);
-        Node & left(const Node n);
+        BTNode(T e,BTNode<T> *p):TNode<T>(e) {parent = p;left = right = 0;}
+        BTNode<T> *parent,*left,*right;
 };
-BinaryTree::BinaryTree() {
 
+template<typename T>
+class BinaryTree : public Tree<T>
+{
+    public:
+        BTNode<T> *root;
+        BinaryTree();
+        void add_root(const T &item);
+        ////Node add_left(const Node & n,const T &item);
+        //Node root();
+        //Node left(const Node n);
+};
+
+template<typename T>
+BinaryTree<T>::BinaryTree()
+{
+    root = 0;
 }
+
+template<typename T>
+void BinaryTree<T>::add_root(const T &item)
+{
+     if(this->size==0)
+     {
+         root = new BTNode<T>(item,0);
+         this->size++;
+     }
+     else
+         std::cout<<"Already has root!"<<std::endl;
+}
+
 #endif
--- C++/Tree/Tree.h
+++ C++/Tree/Tree.h
@@ -1,13 +1,50 @@
+/**
+  ******************************************************************************
+  * @file    Tree.h
+  * @author  Wayne Hu
+  * @date    2015.09.05
+  * @brief   This file provides all the headers of the menu functions.
+  ******************************************************************************
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef TREE_H_
 #define TREE_H_
 
-class Tree {
+/* Includes ------------------------------------------------------------------*/
+#include <iostream>
 
+
+/* Exported macro ------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+/* Class delcaration ---------------------------------------------------------*/
+/* Template definition -------------------------------------------------------*/
+
+template<typename T>
+class TNode
+{
+    public:
+        TNode(T e) {element = e;}
+        T element;
+};
+
+
+template<typename T>
+class Tree
+{
     public:
         Tree();
-        virtual
+        int size;
 };
-Tree::Tree() {
 
+template<typename T>
+Tree<T>::Tree()
+{
+    size = 0;
 }
+
+
 #endif
