23fe78597b2aa24bfdd08566c636c8d5f70ff87a
waynehu <iamwaynehu@gmail.com>
2015-09-24 19:44:21 +0800

    Wayne:[C++] Modify source code

--- C++/LinkList/DoubleLinkedList.cpp
+++ C++/LinkList/DoubleLinkedList.cpp
@@ -22,23 +22,22 @@ ostream & operator<<(ostream &os,obj &e){
 
 int main(int argc, char const* argv[])
 {
-    auto list = new DoubleLinkedList<int>();
-    list->add_front(5);
-    list->add_front(6);
-    list->add_front(7);
+    auto list = DoubleLinkedList<int>();
+    list.add_front(5);
+    list.add_front(6);
+    list.add_front(7);
 
-    list->show();
-    delete list;
+    list.show();
 
-    auto li = new DoubleLinkedList<obj>();
-    auto r1 = new obj(1,2);
-    auto r2 = new obj(3,4);
-    auto r3 = new obj(5,6);
-    auto r4 = new obj;
-    li->add_front(*r1);
-    li->add_front(*r2);
-    li->add_front(*r3);
-    li->add_front(*r4);
-    li->show();
+    auto li = DoubleLinkedList<obj>();
+    auto r1 = obj(1,2);
+    auto r2 = obj(3,4);
+    auto r3 = obj(5,6);
+    auto r4 = obj();
+    li.add_front(r1);
+    li.add_front(r2);
+    li.add_front(r3);
+    li.add_front(r4);
+    li.show();
     return 0;
 }
